f9db0f24f85c1cc988931261d917f22fa618f470
Victor <victorac2324@gmail.com>
2024-02-01 22:14:21 -0500

    wrote functions to update objects

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee  = { 
+    name: "x",
+    streetAdress: "y",
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newEmployee = {...obj}
+    newEmployee[key] = value;
+    return newEmployee
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+
+    return obj
+}
+
+function deleteFromEmployeeByKey (obj, key,){
+    const firedEmployee = {...obj}
+    delete firedEmployee[key] 
+    return firedEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key]
+    return obj
+}
